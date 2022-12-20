63a046adab77d733e37adee9242f11a377f5af7b
David-Buchukuri <datobuchukuri20@gmail.com>
2022-12-20 23:54:01 +0400

    feat: copy room id wiith mouse

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,21 +2,28 @@
   <main>
     <h1>anonymous chat</h1>
     <section>
-      <div>
+      <div class="d-flex-col-gap">
         <h3>Create Your Own Chat Room</h3>
         <button @click="createRoom">Create Chat Room</button>
-        <span v-if="roomIdFromResponse"
-          >chat room id: {{ roomIdFromResponse }}</span
-        >
+        <div v-if="roomIdFromResponse" class="room-id-input-wrapper">
+          <span>chat room id: {{ roomIdFromResponse }}</span>
+          <CopyIcon
+            @click="copyRoomId"
+            :color="copyIconColor"
+            class="copy-icon"
+          />
+        </div>
       </div>
-      <div>
+      <div class="d-flex-col-gap">
         <h3>Already Have A Code?</h3>
         <h3>Join The Room!</h3>
+
         <input
           type="text"
           placeholder="enter room id"
           v-model="roomIdFromInput"
         />
+
         <span v-if="error" class="error"
           >room id is invalid or the room is full
         </span>
@@ -30,12 +37,20 @@
 import { ref } from "vue";
 import { useRouter } from "vue-router";
 import { state } from "../stores/wsStore";
+import CopyIcon from "@/components/icons/CopyIcon.vue";
 
 const router = useRouter();
 
 const error = ref(false);
 const roomIdFromInput = ref(null);
 const roomIdFromResponse = ref(null);
+const copyIconColor = ref("black");
+
+const copyRoomId = () => {
+  copyIconColor.value = "green";
+  setTimeout(() => (copyIconColor.value = "black"), 2000);
+  navigator.clipboard.writeText(roomIdFromResponse.value);
+};
 
 const createRoom = async () => {
   const responsePromise = await fetch("http://127.0.0.1:8005/create-room");
@@ -69,7 +84,7 @@ const joinChat = () => {
   color: red;
 }
 
-div {
+.d-flex-col-gap {
   display: flex;
   justify-content: center;
   align-items: center;
@@ -103,5 +118,21 @@ main {
   flex-direction: column;
   gap: 200px;
   height: 80vh;
+}
+
+.copy-icon {
+  width: 22px;
+  opacity: 0.9;
+  cursor: pointer;
+}
+
+.copy-icon:hover {
+  scale: 1.07;
+}
+
+.room-id-input-wrapper {
+  display: flex;
+  gap: 20px;
+  align-items: center;
 }
 </style>
